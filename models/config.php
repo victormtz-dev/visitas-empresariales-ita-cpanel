e5fa1f73ee64bb5ab1fa2613ba937718e68fecef
victormtz-dev <victormartinezalvarado@outlook.com>
2022-05-26 10:17:58 -0500

    Agregando nuevos cambios en admin

--- models/config.php
+++ models/config.php
@@ -1,15 +1,16 @@
 <?php
-/*
+
 $host="localhost";
 $port="3306";
 $database ="conveni2_visitas";
 $user ="conveni2_gestion";
 $pass ="vinculacion*12";
-*/
+
+/*
 $host="localhost";
 $port="3306";
 $database ="tecnm_ita_visitas";
 $user ="root";
 $pass ="";
-
+*/
 ?>
